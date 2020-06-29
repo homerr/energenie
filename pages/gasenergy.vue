8de21fbf53bd6940430958fada90b8a56b21ec7d
homerr <19776252+homerr@users.noreply.github.com>
2020-06-29 08:12:17 +0100

    added logic for cuft-m3, tidied up crap

--- pages/gasenergy.vue
+++ pages/gasenergy.vue
@@ -2,40 +2,44 @@
     <section>
     <div class="row text-center">
         <div class="col-sm-12 mb-3">
-            <h1>Gas Calculation (m3 to killowatt-hours)</h1>
+            <h1>Gas Consumption to killowatt-hours</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 d-flex justify-content-center mb-3">
-            <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-secondary active">
-                    <input type="radio" name="gasmeasure" id="gas-m3-opt" autocomplete="off" checked>m3</label>
-                <label class="btn btn-secondary">
-                    <input type="radio" name="gasmeasure" id="gas-cuft-opt" autocomplete="off">cuft3</label>
-            </div>
+        <div class="col-sm-12 d-flex justify-content-center align-text-center mb-3">
+            <b-form-group>
+                <b-form-radio-group
+                id="meter-radios"
+                v-model="selected"
+                :options="options"
+                buttons
+                button-variant="outline-primary"
+                name="radios-metering"
+                ></b-form-radio-group>
+            </b-form-group>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 d-flex justify-content-center">
+        <div class="col-sm-12 d-flex justify-content-center text-center">
             <form>
             <div class="form-group">
                 <label for="kWhReading">Opening Reading</label>
-                <input type="number" name="gas-open" id="gas-open" v-model="gasOpen">
+                <input type="number" name="gas-open" class="form-control" id="gas-open" v-model="gasOpen">
             </div>
             <div class="form-group">
                 <label for="kWhReading">Closing Reading</label>
-                <input type="number" name="gas-close" id="gas-close" v-model="gasClose">
+                <input type="number" name="gas-close" class="form-control" id="gas-close" v-model="gasClose">
             </div>
             <div class="form-group">
-                <label for="kWhReading">Gas M3 Total</label>
-                <input type="number" name="gas-m3" id="gas-m3" v-model="gasM3">
+                <label for="kWhReading">Gas Total</label>
+                <input type="number" name="gas-tot" class="form-control" id="gas-tot" v-model="gasTot">
             </div>
             <div class="form-group">
                 <label for="calValue">Calorific Value</label>
-                <input type="number" name="gas-cal" id="gas-cal" v-model="gasCal">
+                <input type="number" name="gas-cal" class="form-control" id="gas-cal" v-model="gasCal">
             </div>
             <div class="m-2">
-                kWh Consumption: {{ gasKwh }}
+                kWh Consumption: {{ gasKwh }} Mode: {{ selected }}
             </div>
             </form>
         </div>
@@ -47,40 +51,33 @@
 export default {
     layout: 'default',
     transition: 'test',
-    methods: {
-        doMaths: function() {
-            alert('nooooo')
-        },
-        tryme () {
-            console.log('moobaa')
-        },
-        combineFunction: function() {
-            var v = this;
-                v.test = v.first.concat(v.last);
-        },
-    },
     head: {
         title: 'Moobaa'
     },
     data () {
         return {
-            "msg": "hello",
-            "first": '',
-            "last": '',
-            "test": '',
+            "selected": 'm3',
             "confactor": '1.02264',
+            "cubic": '0.0283',
             "gasCal": '39.9',
             "convFactor": '3.6',
             "gasOpen": '',
-            "gasClose": ''
-    }
+            "gasClose": '',
+        options: [
+            { text: 'M3', value: 'm3'},
+            { text: 'cuft', value: 'cuft'}
+        ]}
     },
     computed: {
         gasKwh: function() {
-            return this.gasM3 * this.confactor * this.gasCal / this.convFactor
+            if (this.selected = 'm3') {
+                return this.gasTot * this.confactor * this.gasCal / this.convFactor
+            } else if (this.selected = 'cuft') {
+                return this.gasTot * this.cubic * this.confactor * this.gasCal / this.convFactor
+            }
         },
-        gasM3: function() {
-            return this.gasClose - this.gasOpen 
+        gasTot: function() {
+            return this.gasClose - this.gasOpen
         }
     }
 }
